97209985b6862a0696fc31d016c7b6cc396aa69e
KoatKoetl <stas20027@outlook.com>
2023-12-07 01:54:43 +0200

    Fixed burger menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './CSS/style.css';
 // Load all assets
 require.context('../src/assets', true);
+import { actionHamburgerMenu, openHamburgerMenuLink, hideHamburgerMenuOnResize } from './burger-menu';
+
+actionHamburgerMenu();
+openHamburgerMenuLink();
+hideHamburgerMenuOnResize();
